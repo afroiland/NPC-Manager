6e7cb2b4b6ac155cd446e98e8a224c95d32bed5c
Andrew Froiland <andrew.froiland@gmail.com>
2017-03-13 18:34:56 -0500

    Add button to random that randomly generates class

--- public/scripts/controllers/random.controller.js
+++ public/scripts/controllers/random.controller.js
@@ -2,13 +2,19 @@ app.controller('RandomController', ['$http', '$location', function($http, $locat
   console.log("Random Controller Running");
   const self = this;
 
+  self.newnpc = {};
+
   var classes=["Fighter", "Cleric", "Mage", "Thief", "Monk", "Druid", "Paladin", "Ranger", "Illusionist", "Assassin"]
   var clSpellsLv1=["cure light wounds", "light", "bless", "command", "create water", "detect magic", "protection from evil", "purify food and drink", "remove fear", "sanctuary"]
   var clSpellsLv2=["chant", "detect charm", "hold person", "know alignment", "resist fire", "silence 15' radius", "slow poison", "snake charm", "speak with animals", "spiritual hammer"]
   var muSpellsLv1=["burning hands", "charm person", "sleep", "magic missile", "detect magic", "light", "shocking grasp", "feather fall", "shield", "protection from evil"]
 
 
-
+  self.generate = function() {
+    // console.log("test");
+    self.newnpc.class = classes[Math.floor(Math.random() * 10)];
+    console.log("self.newnpc: ", self.newnpc);
+  };
 
 
 
