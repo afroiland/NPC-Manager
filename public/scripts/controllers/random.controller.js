f929a0f950c2f335bdaca297d1f6ed42a01559de
Andrew Froiland <andrew.froiland@gmail.com>
2017-04-07 09:07:22 -0500

    Reset ex_str for classes that don't get it

--- public/scripts/controllers/random.controller.js
+++ public/scripts/controllers/random.controller.js
@@ -11,8 +11,8 @@ app.controller('RandomController', ['$http', '$location', function($http, $locat
   var muSpellsLv1=["burning hands", "charm person", "sleep", "magic missile", "detect magic", "light", "shocking grasp", "feather fall", "shield", "protection from evil"]
 
   self.generate = function() {
-    // switch (Math.floor(Math.random() * 10)) {
-    switch (3) {
+    switch (Math.floor(Math.random() * 5)) {
+    // switch (3) {
       case 0:
       generateFighter();
       break;
@@ -56,6 +56,7 @@ app.controller('RandomController', ['$http', '$location', function($http, $locat
     self.newnpc.str = 0;
     for (i = 0; self.newnpc.str < 9; i++) {
       self.newnpc.str = (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1);
+      // Check for ex_str
       if (self.newnpc.str == 18) {
         self.newnpc.ex_str = Math.floor(Math.random() * 100);
       } else {
@@ -104,6 +105,7 @@ app.controller('RandomController', ['$http', '$location', function($http, $locat
 
     // Set cleric attributes (Wis must be 9+)
     self.newnpc.str = (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1);
+    self.newnpc.ex_str = 0;
     self.newnpc.int = (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1);
     self.newnpc.dex = (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1);
     self.newnpc.con = (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1);
@@ -147,6 +149,7 @@ app.controller('RandomController', ['$http', '$location', function($http, $locat
 
     // Set mage attributes (Int must be 9+)
     self.newnpc.str = (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1);
+    self.newnpc.ex_str = 0;
     self.newnpc.int = 0;
     for (i = 0; self.newnpc.int < 9; i++) {
       self.newnpc.int = (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1);
@@ -189,6 +192,7 @@ app.controller('RandomController', ['$http', '$location', function($http, $locat
 
     // Set thief attributes (Dex must be 9+)
     self.newnpc.str = (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1);
+    self.newnpc.ex_str = 0;
     self.newnpc.int = (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1);
     self.newnpc.dex = 0
     for (i = 0; self.newnpc.dex < 9; i++) {
@@ -241,6 +245,7 @@ app.controller('RandomController', ['$http', '$location', function($http, $locat
 
     // Set monk attributes (several requirements)
     self.newnpc.str = 0;
+    self.newnpc.ex_str = 0;
     for (i = 0; self.newnpc.str < 9; i++) {
       self.newnpc.str = (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1);
     }
@@ -283,6 +288,7 @@ app.controller('RandomController', ['$http', '$location', function($http, $locat
 
     // Set cleric attributes
     self.newnpc.str = 0;
+    self.newnpc.ex_str = 0;
     for (i = 0; self.newnpc.str < 9; i++) {
       self.newnpc.str = (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1);
     }
@@ -367,6 +373,7 @@ app.controller('RandomController', ['$http', '$location', function($http, $locat
 
     // Set cleric attributes
     self.newnpc.str = 0;
+    self.newnpc.ex_str = 0;
     for (i = 0; self.newnpc.str < 9; i++) {
       self.newnpc.str = (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1) + (Math.floor(Math.random() * 6) + 1);
     }
