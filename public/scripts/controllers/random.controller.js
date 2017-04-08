86fec2775711378b4c031d5972c7d6d706af53d8
Andrew Froiland <andrew.froiland@gmail.com>
2017-04-08 08:26:09 -0500

    Remove unnecessary code

--- public/scripts/controllers/random.controller.js
+++ public/scripts/controllers/random.controller.js
@@ -5,14 +5,13 @@ app.controller('RandomController', ['$http', '$location', function($http, $locat
   self.newnpc = {};
   self.showcard = false;
 
-  var classes=["Fighter", "Cleric", "Magic-User", "Thief", "Monk", "Druid", "Paladin", "Ranger", "Illusionist", "Assassin"]
   var clSpellsLv1=["cure light wounds", "light", "bless", "command", "create water", "detect magic", "protection from evil", "purify food and drink", "remove fear", "sanctuary"]
   var clSpellsLv2=["chant", "detect charm", "hold person", "know alignment", "resist fire", "silence 15' radius", "slow poison", "snake charm", "speak with animals", "spiritual hammer"]
   var muSpellsLv1=["burning hands", "charm person", "sleep", "magic missile", "detect magic", "light", "shocking grasp", "feather fall", "shield", "protection from evil"]
 
   self.generate = function() {
-    // switch (Math.floor(Math.random() * 5)) {
-    switch (2) {
+    switch (Math.floor(Math.random() * 5)) {
+    // switch (2) {
       case 0:
       generateFighter();
       break;
