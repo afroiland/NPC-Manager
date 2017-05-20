fdfaa0131775232e1f0b3dfe64af5952eeb267b2
Andrew Froiland <andrew.froiland@gmail.com>
2017-05-20 07:44:22 -0500

    Begin creating combat function

--- public/scripts/controllers/combat.controller.js
+++ public/scripts/controllers/combat.controller.js
@@ -2,8 +2,27 @@ app.controller('CombatController', ['$http', '$location', function($http, $locat
   console.log("CombatController Running");
   const self = this;
 
+  self.combatant1 = {};
+  self.combatant2 = {};
+  var segment;
+
+  self.fight = function() {
+    // Initiative
+
+    // Advance segments until someone's turn to act
+
+    // Choose action
+
+    // Results
+
+    // continue w/ segments
 
 
+  }
+
+  function intiative() {
+
+  }
 
 
 }]);
