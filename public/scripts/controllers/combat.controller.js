e363882370c050139b42543e139679e2d340a5ca
Andrew Froiland <andrew.froiland@gmail.com>
2017-05-28 14:57:23 -0500

    oneRound function working

--- public/scripts/controllers/combat.controller.js
+++ public/scripts/controllers/combat.controller.js
@@ -3,9 +3,9 @@ app.controller('CombatController', ['$http', '$location', function($http, $locat
   const self = this;
 
   //self.combatant1 = {};
-  self.combatant1 = {name:'Dude', ac:7, currenthp:10, thac0: 20, dmg:'d6'};
+  self.combatant1 = {name:'Dude1', ac:7, currenthp:10, thac0: 20, dmg:'d6'};
   //self.combatant2 = {};
-  self.combatant2 = {name:'Person', ac:7, currenthp:10, thac0: 20, dmg:'d6'};
+  self.combatant2 = {name:'Dude2', ac:7, currenthp:10, thac0: 20, dmg:'d6'};
   var segment = 0;
 
   self.oneRound = function() {
@@ -15,28 +15,32 @@ app.controller('CombatController', ['$http', '$location', function($http, $locat
     console.log('self.combatant1.init: ', self.combatant1.init);
     console.log('self.combatant2.init: ', self.combatant2.init);
     for (i = segment; i < 10; i++) {
-      if (self.combatant1.init == self.combatant2.init) {
+      if (self.combatant1.init == i && self.combatant2.init ==i) {
         console.log('simultaneous');
-        if (Math.floor(Math.random() * 20) + 1 >= self.combatant1.thac0 - self.combatant2.ac) {
-          self.combatant2.currenthp -= //damage;
-        }
-        if (Math.floor(Math.random() * 20) + 1 >= self.combatant2.thac0 - self.combatant1.ac) {
-          self.combatant1.currenthp -= //damage;
-        }
-        if (checkForIncap(self.combatant1.currenthp)) {
+        attack(self.combatant1, self.combatant2);
+        attack(self.combatant2, self.combatant1);
+        if (checkForIncap(self.combatant1)) {
           console.log(self.combatant1.name + 'has fallen');
           fightOver = true;
         }
-        if (checkForIncap(self.combatant2.currenthp)) {
+        if (checkForIncap(self.combatant2)) {
           console.log(self.combatant2.name + 'has fallen');
           fightOver = true;
         }
       } else if (self.combatant1.init == i) {
         console.log('combatant1 go');
         attack(self.combatant1, self.combatant2);
+        if (checkForIncap(self.combatant2)) {
+          console.log(self.combatant2.name + 'has fallen');
+          fightOver = true;
+        }
       } else if (self.combatant2.init == i) {
         console.log('combatant2 go');
         attack(self.combatant2, self.combatant1);
+        if (checkForIncap(self.combatant1)) {
+          console.log(self.combatant1.name + 'has fallen');
+          fightOver = true;
+        }
       }
       if (fightOver) {
         return;
@@ -46,7 +50,7 @@ app.controller('CombatController', ['$http', '$location', function($http, $locat
 
   self.toTheFinish = function() {
     console.log('fighting to the finish');
-  }
+  };
 
   function determineInit() {
     console.log('determining init');
@@ -55,14 +59,19 @@ app.controller('CombatController', ['$http', '$location', function($http, $locat
   }
 
   function attack(attacker, defender) {
-    console.log(attacker.name + ' is attacking ' + defender.name);
+    //console.log(attacker.name + ' is attacking ' + defender.name);
     if (Math.floor(Math.random() * 20) + 1 >= attacker.thac0 - defender.ac) {
-      defender.currenthp -= //damage;
+      var dmg = Math.floor(Math.random() * 6) + 1;
+      defender.currenthp -= dmg;
+      console.log(attacker.name + ' hits, inflicting ' + dmg + ' damage. ' + defender.name + ' is at ' + defender.currenthp + 'hp.');
+    } else {
+      console.log('Miss');
     }
   }
 
   function checkForIncap(char) {
-    if (char.hp <= 0) {
+    console.log('checking for incap');
+    if (char.currenthp <= 0) {
       return true;
     }
   }
