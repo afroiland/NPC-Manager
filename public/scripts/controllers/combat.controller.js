4be7df45db6ed965c6c81a0e38ea3e2bb4e562e5
Andrew Froiland <andrew.froiland@gmail.com>
2017-05-27 19:49:46 -0500

    Working on oneRound function

--- public/scripts/controllers/combat.controller.js
+++ public/scripts/controllers/combat.controller.js
@@ -2,16 +2,25 @@ app.controller('CombatController', ['$http', '$location', function($http, $locat
   // console.log("CombatController Running");
   const self = this;
 
-  self.combatant1 = {};
-  self.combatant2 = {};
+  //self.combatant1 = {};
+  self.combatant1 = {ac:7, currenthp:10, thac0: 10}
+  //self.combatant2 = {};
+  self.combatant2 = {ac:7, currenthp:10, thac0: 10}
   var segment = 0;
 
   self.oneRound = function() {
+    console.log('fighting a bit');
     determineInit();
-    if (self.combatant1.init == self.combatant2.init) {
-      console.log('tied');
-    } else {
-      console.log('not tied');
+    console.log('self.combatant1.init: ', self.combatant1.init);
+    console.log('self.combatant2.init: ', self.combatant2.init);
+    for (i = segment; i < 10; i++) {
+      if (self.combatant1.init == self.combatant2.init) {
+        console.log('tied');
+      } else if (self.combatant1.init == i) {
+        console.log('combatant1 go');
+      } else if (self.combatant2.init == i) {
+        console.log('combatant2 go');
+      }
     }
 
     // Advance segments until someone's turn to act
@@ -26,6 +35,7 @@ app.controller('CombatController', ['$http', '$location', function($http, $locat
   }
 
   function determineInit() {
+    console.log('determining init');
     self.combatant1.init = Math.floor(Math.random() * 6) + 1;
     self.combatant2.init = Math.floor(Math.random() * 6) + 1;
   }
